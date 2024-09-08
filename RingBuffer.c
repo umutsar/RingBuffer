5943845bb1cd2a79991447e0821fbd1866f4e3d4
Umut Şar <contact@umutsar.net>
2024-09-09 01:47:16 +0300

    Update RingBuffer.c

--- RingBuffer.c
+++ RingBuffer.c
@@ -1,3 +1,11 @@
+/* 
+    This code is optimized for microcontroller boards. Adjust the uint_size
+    type according to your project goals. If you have a microcontroller project
+    and plan to use communication protocols such as UART, I2C, or CAN, you need
+    to use 8-bit data, as these protocols support it. Therefore, be mindful of
+    the data type.
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
